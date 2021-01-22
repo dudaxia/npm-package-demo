f366d3aade6272a22ae93c5a009416af69e2df3e
dudaxia <1729453966@qq.com>
2021-01-22 11:16:10 +0800

    feat:init

---
+++ examples/src/index.js
@@ -0,0 +1,23 @@
+// 直接引入原文件
+import { Stack } from '../../src/Stack'
+
+var stack = new Stack()
+for (var i = 0; i < 5; i++) {
+    stack.push(i)
+}
+console.log('isEmpty: ', stack.isEmpty)
+console.log('size: ', stack.size)
+console.log(stack.toString())
+console.log(`pop: `, stack.pop())
+
+stack.traverse((ele,index)=>{
+    console.log(`Traversing-Stack:${index}: ${ele}`)
+})
+
+stack.traverse((ele,index)=>{
+    console.log(`Traversing-Stack:${index}: ${ele}`)
+}, true)
+
+console.log(`claer: `, stack.clear())
+console.log('isEmpty: ', stack.isEmpty)
+console.log('size: ', stack.size)
